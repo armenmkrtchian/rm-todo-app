01c7dca740206841f5236e90cbb3c4ccd8561e55
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2019-12-06 23:35:49 +0400

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Navbar} from './src/Navbar'
+import { AddTodo } from './src/AddTodo';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Navbar title="Todo app!"/>
+      <AddTodo/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
---
+++ src/AddTodo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {View, StyleSheet, TextInput, Button} from 'react-native'
+
+export const AddTodo = props => {
+    return (
+        <View style={styles.block}>
+            <TextInput style={styles.input}/>
+            <Button title='Add'/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '70%',
+        borderStyle: 'solid',
+        borderBottomWidth: 10,
+        borderBottomColor: '#3949ab'
+    }
+})
