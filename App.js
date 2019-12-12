fcaa9cbd5970d273d995925890eeb1fd945eba9e
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2019-12-13 01:01:12 +0400

    added lists rendering

--- App.js
+++ App.js
@@ -1,45 +1,41 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Navbar} from './src/Navbar'
-import { AddTodo } from './src/AddTodo';
-import { Todo } from './src/Todo';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddTodo";
+import { Todo } from "./src/Todo";
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([
+    { id: 1, title: "test" },
+    { id: 2, title: "test" },
+    { id: 3, title: "test" },
+    { id: 4, title: "test" },
+    { id: 5, title: "test" },
+    { id: 6, title: "test" },
+    { id: 7, title: "test" }
+  ]);
 
   const addTodo = title => {
-    // const newTodo = {
-    //   id: Date.now().toString(),
-    //   title: title
-    // }
-
-    // setTodos(todos.concat([ newTodo ]))
-    // setTodos((prevTodos) => {
-    //   return [
-    //     ...prevTodos,
-    //     newTodo
-    //   ]
-    // })
-
     setTodos(prev => [
       ...prev,
       {
         id: Date.now().toString(),
         title
       }
-    ])
-  }
+    ]);
+  };
+
   return (
     <View>
-      <Navbar title="Todo app!"/>
+      <Navbar title="Todo app!" />
       <View style={styles.container}>
-        <AddTodo onSubmit={addTodo}/>
+        <AddTodo onSubmit={addTodo} />
 
-        <View>
-          {todos.map(todo => (
-            <Todo todo={todo} key={todo.id} />
-          ))}
-        </View>
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+        />
       </View>
     </View>
   );
