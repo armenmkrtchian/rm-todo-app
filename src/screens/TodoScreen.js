83cec275f5a5ba8e681a57c4a7cbffca8e92b2ee
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2019-12-15 01:28:11 +0400

    card visual changes

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import { THEME } from '../theme'
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { THEME } from "../theme";
+import { AppCard } from "../components/ui/AppCard";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title="Edit" />
+      </AppCard>
+
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button title='Back' onPress={goBack} color={THEME.GREY_COLOR} />
+          <Button title="Back" onPress={goBack} color={THEME.GREY_COLOR} />
         </View>
         <View style={styles.button}>
           <Button
-            title='Delete'
+            title="Delete"
             color={THEME.DANGER_COLOR}
-            onPress={() => console.log('To Remove')}
+            onPress={() => console.log("To Remove")}
           />
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
   },
   button: {
-    width: '40%'
+    width: "40%"
+  },
+  title: {
+    fontSize: 20
   }
-})
+});
